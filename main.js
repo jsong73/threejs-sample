71218096e62705271ec9efb5dbed3f68148cf42f
jsong73 <jessicajinsong@gmail.com>
2023-07-10 21:30:16 -0400

    click

--- main.js
+++ main.js
@@ -1,37 +1,86 @@
 import * as THREE from "three";
+import * as TWEEN from '@tweenjs/tween.js';
 
-//creating the scene
+// Creating the scene
 const scene = new THREE.Scene();
-// creating the camera
-const camera = new THREE. PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-// creating the renderer
+// Creating the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// Creating the renderer
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('canvas-container').appendChild(renderer.domElement);
 
-// creating light
+// Creating light
 const light = new THREE.PointLight(0xFFFFFF, 1);
 light.position.set(0, 0, 5);
 scene.add(light);
 
-// cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 });
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+camera.position.set(0, 0, 7);
+const targetPosition = new THREE.Vector3(0, 0, 0);
+camera.lookAt(targetPosition);
 
-camera.position.z = 5;
+const laptopWidth = 3;
+const laptopHeight = 0.2;
+const laptopDepth = 2;
+const laptopColor = 0x808080;
 
-// rendering the scene
-function animate () {
-    requestAnimationFrame( animate );
+const laptopGeometry = new THREE.BoxGeometry(laptopWidth, laptopHeight, laptopDepth);
+const laptopMaterial = new THREE.MeshStandardMaterial({ color: laptopColor });
+const laptop = new THREE.Mesh(laptopGeometry, laptopMaterial);
+scene.add(laptop);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
-    renderer.render( scene, camera );
+// Set initial rotation of the laptop
+laptop.rotation.x = 0;
+
+// Animation parameters
+const openRotation = -Math.PI / 2; // Open rotation angle
+const closeRotation = 0; // Close rotation angle
+const duration = 1000; // Animation duration in milliseconds
+
+// Function to open the laptop
+function openLaptop() {
+  new TWEEN.Tween(laptop.rotation)
+    .to({ x: openRotation }, duration)
+    .easing(TWEEN.Easing.Quadratic.InOut)
+    .start();
+}
+
+// Function to close the laptop
+function closeLaptop() {
+  new TWEEN.Tween(laptop.rotation)
+    .to({ x: closeRotation }, duration)
+    .easing(TWEEN.Easing.Quadratic.InOut)
+    .start();
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize);
+
+function onMouseClick(event) {
+  const canvasBounds = renderer.domElement.getBoundingClientRect();
+  const clickX = event.clientX - canvasBounds.left;
+  const clickY = event.clientY - canvasBounds.top;
+  if (clickX >= 0 && clickX < canvasBounds.width && clickY >= 0 && clickY < canvasBounds.height) {
+    if (laptop.rotation.x === closeRotation) {
+      openLaptop();
+    } else {
+      closeLaptop();
+    }
+  }
+}
+
+window.addEventListener('click', onMouseClick);
+
+// Rendering the scene
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  TWEEN.update();
 }
 
 animate();
-
-
