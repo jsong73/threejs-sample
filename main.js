940204b29a3667b2c236694c2245ea1d21e00dc9
jsong73 <jessicajinsong@gmail.com>
2023-07-10 20:40:07 -0400

    added lighting

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ import * as THREE from "three";
 
 //creating the scene
 const scene = new THREE.Scene();
+// creating the camera
 const camera = new THREE. PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+// creating the renderer
 const renderer = new THREE.WebGLRenderer();
-
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+document.getElementById('canvas-container').appendChild(renderer.domElement);
+
+// creating light
+const light = new THREE.PointLight(0xFFFFFF, 1);
+light.position.set(0, 0, 5);
+scene.add(light);
 
 // cube
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
