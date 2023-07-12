bf4d2d57c28b5b91525e314596522cf1b6eb5819
jsong73 <jessicajinsong@gmail.com>
2023-07-12 16:30:22 -0400

    zoom working

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 // Creating the renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.domElement.style.pointerEvents = "auto";
+
 document.getElementById("container").appendChild(renderer.domElement);
 
 // Creating light
@@ -18,27 +21,60 @@ const light = new THREE.PointLight(0xffffff, 2);
 light.position.set(0, 2, 2);
 scene.add(light);
 
-//Set camera position
+// Set camera position
 camera.position.set(0, 1, 2);
 const targetPosition = new THREE.Vector3(0, 0, 0);
 camera.lookAt(targetPosition);
 
 //Laptop Model loader
 const loader = new GLTFLoader();
+let laptopModel = null; 
+
 loader.load(
     "/public/models/poly_laptop.gltf",
-    function(gltf){
-        const laptopModel = gltf.scene;
-        //scaling the laptop model in size
-        laptopModel.scale.set(3, 3, 3); 
-        scene.add(laptopModel)
+    function (gltf) {
+      laptopModel = gltf.scene.children[0];
+      //scaling the laptop model in size
+      laptopModel.scale.set(3, 3, 3);
+      scene.add(laptopModel);
     },
     undefined,
-    function(error){
-        console.log(error)
+    function (error) {
+      console.log(error);
     }
-);
+  );
 
+const zoomDuration = 1000;
+const zoomDistance = 1.0;
+  
+function onMouseClick(event) {
+    const canvasBounds = renderer.domElement.getBoundingClientRect();
+    const mouse = new THREE.Vector2(
+    ((event.clientX - canvasBounds.left) / canvasBounds.width) * 2 - 1,
+    -((event.clientY - canvasBounds.top) / canvasBounds.height) * 2 + 1
+);
+  
+const raycaster = new THREE.Raycaster();
+raycaster.setFromCamera(mouse, camera);
+  
+const intersects = raycaster.intersectObject(laptopModel, true);
+  
+if (intersects.length > 0) {
+    const currentCameraPosition = camera.position.clone();
+      const targetCameraPosition = intersects[0].point.clone().add(new THREE.Vector3(0, 0, zoomDistance));
+  
+      new TWEEN.Tween(currentCameraPosition)
+        .to(targetCameraPosition, zoomDuration)
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .onUpdate(() => {
+          camera.position.copy(currentCameraPosition);
+          camera.lookAt(targetPosition);
+        })
+        .start();
+    }
+  }
+  
+window.addEventListener("click", onMouseClick);
 
 
 // Rendering the scene
