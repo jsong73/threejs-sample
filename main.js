1be0278ad1a8621e30487d76d7459fcbc2406082
jsong73 <jessicajinsong@gmail.com>
2023-07-12 15:21:00 -0400

    working on lighting and cam set up

--- main.js
+++ main.js
@@ -4,22 +4,26 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
 
 // Creating the scene
 const scene = new THREE.Scene();
+
 // Creating the camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 // Creating the renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('canvas-container').appendChild(renderer.domElement);
+document.getElementById("container").appendChild(renderer.domElement);
 
 // Creating light
-const light = new THREE.PointLight(0xFFFFFF, 1);
-light.position.set(0, 0, 5);
+const light = new THREE.PointLight(0xffffff, 2);
+light.position.set(0, 2, 2);
 scene.add(light);
 
-camera.position.set(0, 0, 2);
+//Set camera position
+camera.position.set(0, 1, 2);
 const targetPosition = new THREE.Vector3(0, 0, 0);
 camera.lookAt(targetPosition);
 
+//Laptop Model loader
 const loader = new GLTFLoader();
 loader.load(
     "/public/models/poly_laptop.gltf",
@@ -34,6 +38,7 @@ loader.load(
         console.log(error)
     }
 );
+
 
 
 // Rendering the scene
