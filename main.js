a55a05689b4383d655ae3c459173c703f50521ff
jsong73 <jessicajinsong@gmail.com>
2023-07-12 15:06:40 -0400

    scaled laptop

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import * as TWEEN from '@tweenjs/tween.js';
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
 
 // Creating the scene
 const scene = new THREE.Scene();
@@ -15,66 +16,25 @@ const light = new THREE.PointLight(0xFFFFFF, 1);
 light.position.set(0, 0, 5);
 scene.add(light);
 
-camera.position.set(0, 0, 7);
+camera.position.set(0, 0, 2);
 const targetPosition = new THREE.Vector3(0, 0, 0);
 camera.lookAt(targetPosition);
 
-const laptopWidth = 3;
-const laptopHeight = 0.2;
-const laptopDepth = 2;
-const laptopColor = 0x808080;
-
-const laptopGeometry = new THREE.BoxGeometry(laptopWidth, laptopHeight, laptopDepth);
-const laptopMaterial = new THREE.MeshStandardMaterial({ color: laptopColor });
-const laptop = new THREE.Mesh(laptopGeometry, laptopMaterial);
-scene.add(laptop);
-
-// Set initial rotation of the laptop
-laptop.rotation.x = 0;
-
-// Animation parameters
-const openRotation = -Math.PI / 2; // Open rotation angle
-const closeRotation = 0; // Close rotation angle
-const duration = 1000; // Animation duration in milliseconds
-
-// Function to open the laptop
-function openLaptop() {
-  new TWEEN.Tween(laptop.rotation)
-    .to({ x: openRotation }, duration)
-    .easing(TWEEN.Easing.Quadratic.InOut)
-    .start();
-}
-
-// Function to close the laptop
-function closeLaptop() {
-  new TWEEN.Tween(laptop.rotation)
-    .to({ x: closeRotation }, duration)
-    .easing(TWEEN.Easing.Quadratic.InOut)
-    .start();
-}
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-window.addEventListener('resize', onWindowResize);
-
-function onMouseClick(event) {
-  const canvasBounds = renderer.domElement.getBoundingClientRect();
-  const clickX = event.clientX - canvasBounds.left;
-  const clickY = event.clientY - canvasBounds.top;
-  if (clickX >= 0 && clickX < canvasBounds.width && clickY >= 0 && clickY < canvasBounds.height) {
-    if (laptop.rotation.x === closeRotation) {
-      openLaptop();
-    } else {
-      closeLaptop();
+const loader = new GLTFLoader();
+loader.load(
+    "/public/models/poly_laptop.gltf",
+    function(gltf){
+        const laptopModel = gltf.scene;
+        //scaling the laptop model in size
+        laptopModel.scale.set(3, 3, 3); 
+        scene.add(laptopModel)
+    },
+    undefined,
+    function(error){
+        console.log(error)
     }
-  }
-}
+);
 
-window.addEventListener('click', onMouseClick);
 
 // Rendering the scene
 function animate() {
